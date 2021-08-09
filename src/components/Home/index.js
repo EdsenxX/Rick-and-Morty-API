a6fc82beced0765cd8519d3b7f13aeb8ccb36ef3
Eduardo Serrano <eduardoserranojaime@gmail.com>
2021-08-09 12:40:07 -0500

    se agrego componente de informacion de cada personaje

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,32 @@
 /* ------------------------------ Dependencies ------------------------------ */
 import React from 'react';
 /* --------------------------------- Assets --------------------------------- */
-import styles from "../../assets/styles/Home/index.module.scss";
+import styles from '../../assets/styles/Home/index.module.scss';
 
 const index = (props) => {
+	const { characters: { results } } = props;
 
-    const {characters: {results}} = props;
-    
-    if(!results){
-        return null;
-    }
-    return (
-        <div id="charactersContainer">
-            <div className={styles.content}>
-                {results.map(character=>(
-                    <img key={character.id} src={character.image} alt={character.name}/>
-                ))}
-            </div>
-        </div>
-    );
-}
+	if (!results) {
+		return null;
+	}
+
+	return (
+		<div className={styles.content}>
+			{results.map((character) => (
+				<div className={styles.character} key={character.id}>
+					<img src={character.image} alt={character.name} />
+					<div className={styles.info}>
+						<p className={styles.name}>{character.name}</p>
+                        <p>{character.status} - {character.species}</p>
+                        <div className={styles.info__field}>
+                            <p>Last Known location:</p>
+                            <p>{character.location.name}</p>
+                        </div>
+					</div>
+				</div>
+			))};
+		</div>
+	);
+};
 
 export default index;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,54 +1,50 @@
 /* ------------------------------ Dependencies ------------------------------ */
-import React, {Component} from 'react';
-import {connect} from "react-redux";
-import * as charactersActions from "../../actions/charactersActions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as charactersActions from '../../actions/charactersActions';
 /* ------------------------------- Components ------------------------------- */
-import Child from "../../components/Home";
+import Child from '../../components/Home';
 
 class index extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			page: 1
+		};
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            page: 1
-        }
-    }
+	componentDidMount = async () => {
+		if (!this.props.characters.length) {
+			await this.props.traerCharacters();
+		}
+		window.addEventListener('scroll', this.endPage);
+	};
 
-    componentDidMount = async () => {
-        if (!this.props.characters.length) {
-            await this.props.traerCharacters();
-        }
-        window.addEventListener("scroll",this.endPage)
-    }
+	endPage = async () => {
+		if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+			let page = this.state.page;
+			if (!page) {
+				page = 0;
+			}
+			if (this.props.characters.length && this.props.characters.info.next) {
+				alert('es todo wey');
+				return;
+			}
+			page++;
+			this.setState({ page });
+			this.props.traerCharacters(page);
+		}
+	};
 
-    endPage = async () => {
-        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-            let page = this.state.page;
-            if(!page){
-                page = 0;
-            }
-            if(this.props.characters.length && this.props.characters.info.next){
-                alert("es todo wey");
-                return
-            }
-            page++;
-            this.setState({page});
-            this.props.traerCharacters(page);
-        }
-    }
+	changePage = (page) => {
+		this.props.traerCharactersChangePage(page);
+		this.props.history.replace(`/characters/page/${page}`, null);
+	};
 
-    changePage = (page) => {
-        this.props.traerCharactersChangePage(page);
-        this.props.history.replace(`/characters/page/${page}`, null);
-    }
-
-    render() {
-        const {characters} = this.props;
-        return (<Child characters={characters}
-            changePage={
-                this.changePage
-            }/>);
-    }
+	render() {
+		const { characters } = this.props;
+		return <Child characters={characters} changePage={this.changePage} />;
+	}
 }
-const mapStateToProps = ({charactersReducer}) => charactersReducer;
+const mapStateToProps = ({ charactersReducer }) => charactersReducer;
 export default connect(mapStateToProps, charactersActions)(index);
